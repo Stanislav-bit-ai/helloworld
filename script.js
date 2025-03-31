9c0beed32ced42ec99c3eb3771edbc33fe0f5da8
Stanislav-bit-ai <stanislaw.yakimoff@gmail.com>
2025-03-31 12:33:46 +0500

    сделал задания по js

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
+//задание1
+//let a = 10;
+//alert(a);
+//a = 20;
+//alert(a);
+
+//задание2
+const dateIphone = 2010;
+console.log(dateIphone);
+
+//задание3
+const nameCreatorjs = 'Брендан Эйх';
+console.log(nameCreatorjs);
+
+//задание4
+//let a = 10;
+//let b = 2;
+//alert(a + b);
+//alert(a - b);
+//alert(a * b);
+//alert(a / b);
+
+//задание5
+let result = 2 ** 5;
+alert(result);
+
+//задание6
+let a = 9;
+let b = 2;
+let rest = a % b;
+alert(rest);
+
+//задание7
+let num = 1;
+num  += 5;
+num  -= 3;
+num  *= 7;
+num  /= 3;
+num  += 1;
+num  -= 1;
+alert(num);
+
+//задание8
+let age = prompt(`Сколько вам лет?`);
+alert(age);
+
+//задание9
+const user = {
+    name: `Иван`,
+    age: 18,
+    isAdmin: true,
+}
+
+//задание10
+let userName = prompt(`Твое Имя`);
+alert(`Привет ${userName}!`);

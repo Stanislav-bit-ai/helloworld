28192d551e22f30e6878a74d332775f5a42c8227
Stanislav-bit-ai <stanislaw.yakimoff@gmail.com>
2025-04-01 11:29:43 +0500

    выполнил дополнительное задание по js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //задание1
-//let a = 10;
-//alert(a);
-//a = 20;
-//alert(a);
+let c = 10;
+alert(c);
+c = 20;
+alert(c);
 
 //задание2
 const dateIphone = 2010;
@@ -13,16 +13,16 @@ const nameCreatorjs = 'Брендан Эйх';
 console.log(nameCreatorjs);
 
 //задание4
-//let a = 10;
-//let b = 2;
-//alert(a + b);
-//alert(a - b);
-//alert(a * b);
-//alert(a / b);
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
 
 //задание5
-let result = 2 ** 5;
-alert(result);
+let resultat = 2 ** 5;
+alert(resultat);
 
 //задание6
 let a = 9;
@@ -54,3 +54,17 @@ const user = {
 //задание10
 let userName = prompt(`Твое Имя`);
 alert(`Привет ${userName}!`);
+
+//дополнительное задание
+let numb = prompt(`Пожалуйста, загадайте любое число и введите его здесь:`); 
+alert(numb);
+let number = numb * 2;
+alert(number)
+let summa = number + 10;
+alert(summa);
+let division = summa / 2;
+alert(division);
+let subtraction = division - numb;
+alert(subtraction);
+let result = subtraction;
+alert(result);

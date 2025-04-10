bb12343a25a3ce734ed49654f025118714e01b72
Stanislav-bit-ai <stanislaw.yakimoff@gmail.com>
2025-04-10 19:37:11 +0500

    подправил центрирование, при помощи calc, карточка игры теперь с смайликом.

--- script.js
+++ script.js
@@ -68,3 +68,110 @@ let subtraction = division - numb;
 alert(subtraction);
 let result = subtraction;
 alert(result);*/
+
+//Домашнее задание 2.3
+//задание1
+
+/*let password = `Пароль`;
+let userPass = prompt (`Введите пароль`);
+if (userPass === password) {
+    alert (`Пароль верный`);
+    
+} else {
+    alert (`пароль не верный`);
+}
+
+
+//задание2
+
+let c = 3;
+if (c > 0 && c < 10) {
+    alert(`Верно`);
+} else {
+    alert(`Неверно`);
+}
+
+
+//задание3
+let d = 50;
+let e = 200;
+if (d >= 100 || e >= 100) {
+    alert(`Верно`);
+} else {
+    alert(`Неверно`);
+}
+
+
+//задание4
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
+
+//задание5
+
+let monthNumber = 8;
+switch (monthNumber) {
+    case 3:
+    case 4:
+    case 5:
+        alert('Весна');       
+        break;
+    case 6:
+    case 7:
+    case 8:
+        alert('Лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        alert('Осень');
+        break;
+    case 12:
+    case 1:
+    case 2:
+        alert('Зима');
+        break;
+
+    default:
+        alert('Неверно');
+        break;       
+}
+
+//Дополнительные задания 
+//задание1
+let userInput = prompt("Пожалуйста, введите любое число");
+
+let number = Number(userInput);
+
+if (isNaN(number)) {
+    alert("Будьте внимателней! Введите любое число ");
+} else {
+    
+    if (number % 2 === 0) {
+        alert("Число четное");
+    } else {
+        alert("Число нечетное");
+    }
+}
+
+//задание2
+
+
+let clientOS = 0; 
+let clientDeviceYear = 2016; 
+if (clientOS === 0) {    
+    if (clientDeviceYear < 2015) {
+        alert("Установите облегченную версию приложения для iOS по ссылке.");
+    } else {
+        alert("Установите версию приложения для iOS по ссылке.");
+    }
+} else if (clientOS === 1) {
+    
+    if (clientDeviceYear < 2015) {
+        alert("Установите облегченную версию приложения для Android по ссылке.");
+    } else {
+        alert("Установите версию приложения для Android по ссылке.");
+    }
+} 
+
+*/
